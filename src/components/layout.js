5694daf42a86fcf05e09447573495455a8f2a09e
maerd19 <revolutions_333@hotmail.com>
2020-10-01 09:59:34 -0500

    Helmet y CSS global

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,9 +1,53 @@
 import React from "react"
+import Helmet from "react-helmet"
+import { Global, css } from "@emotion/core"
 import Header from "./header"
 
 const Layout = ({ children }) => {
   return (
     <>
+      <Global
+        styles={css`
+          html {
+            font-size: 62.5%;
+          }
+          body {
+            font-size: 16px;
+            font-size: 1.6rem;
+            line-height: 1.5;
+          }
+          h1,
+          h2,
+          h3 {
+            margin: 0;
+            line-height: 1.5;
+          }
+          h1,
+          h2 {
+            font-family: "Roboto", serif;
+          }
+          h3 {
+            font-family: "PT Sans", sans-serif;
+          }
+          ul {
+            list-style: none;
+            margin: 0;
+            padding: 0;
+          }
+        `}
+      />
+      <Helmet>
+        <title>Gatsby Hotel</title>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css"
+        />
+        <link
+          href="https://fonts.googleapis.com/css?family=PT+Sans:400,700|Roboto:400,700&display=swap"
+          rel="stylesheet"
+        />
+      </Helmet>
+
       <Header />
       {children}
     </>
